b828bcc004583576d37c244bc832844e46a149ea
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-11-16 15:07:17 +0530

    Update compliants.router.js

--- routers/compliants.router.js
+++ routers/compliants.router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
  const authMiddleware =require('../middleware/auth');
 const authController = require('../controllers/auth.controller');
+const complaintsController = require('../controllers/complaint.controller');
 
 // auth
-router.post('/createuser',authController.createUser);
-router.post('/login',authController.login);
+router.post('/createcomplaints',complaintsController.UserComplaintsCreate);
+router.post('/complaintslist',complaintsController.listComplaint);
 router.post('/userlist',authMiddleware,authController.listUser);
 
 
