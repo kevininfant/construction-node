b7e588cb4d86cfcee53164535fba6f7849ca9a97
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-12-11 18:45:49 +0530

    Create assigncomplaint.model.js

---
+++ models/assigncomplaint.model.js
@@ -0,0 +1,33 @@
+const {Sequelize,DataTypes} = require('sequelize');
+const sequelize = require('../config/sequelize');
+const  AssignComplaints = sequelize.define('assigncomplaintdata',{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      complaint_id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      is_active: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue:1
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+module.exports = AssignComplaints;
