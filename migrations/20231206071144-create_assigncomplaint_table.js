a3e08e0b5347d18f46f836e76adea11e2f987f3c
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-12-11 18:46:28 +0530

    Create 20231206071144-create_assigncomplaint_table.js

---
+++ migrations/20231206071144-create_assigncomplaint_table.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('assigncomplaintdata', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      complaint_id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      user_id: {
+        type: Sequelize.DataTypes.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      is_active: {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue:1
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('statuses');
+  }
+};
