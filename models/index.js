8aaed784c396b3407a3dd95277ba6eed424ab04f
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-12-19 19:07:08 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -11,7 +11,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  const config = require("../config/config.js")[env];
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
