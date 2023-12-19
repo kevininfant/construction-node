330ee50f90ddaf397b303e80bfc78309219a12fa
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-12-19 23:34:44 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -4,18 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
-require("dotenv").config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require("../config/config.js")[env];
+const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
-console.log('Config:', config);
+console.log("config",config);
 let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+}
 
 fs
   .readdirSync(__dirname)
@@ -34,11 +33,11 @@ fs
     db[model.name] = model;
   });
 
-  Object.keys(db).forEach(modelName => {
-    if (db[modelName].associate) {
-      db[modelName].associate(db);
-    }
-  });
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
 
 db.sequelize = sequelize;
 
