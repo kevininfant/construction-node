390193427862e39c585e3eb59e0687102bf45bee
kevininfant <53075931+kevininfant@users.noreply.github.com>
2023-12-20 00:23:50 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const process = require('process');
 require("dotenv").config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require("../config/config.js")[env];
+const config = require("../config/config.js")['development'];
 const db = {};
 console.log("config",config);
 let sequelize;
